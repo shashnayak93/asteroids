9662f9deb8d5a1f8d4685fc498fbf0b732ac47cf
shashnayak93 <shashwath.nayak@jellyfish.co>
2025-02-11 23:28:23 -0500

    Created Asteroids and Asteroid Field

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,10 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def collision(self,circle):
+        distance = pygame.math.Vector2.distance_to(self.position,circle.position)
+        if distance <= (self.radius+circle.radius):
+            return True
+        else:
+            return False
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
 from player import Player
 
@@ -10,7 +13,16 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable,drawable)
+    Asteroid.containers = (asteroids,updatable,drawable)
+    AsteroidField.containers = (updatable)
+
     player = Player(x=SCREEN_WIDTH /2,y=SCREEN_HEIGHT/2)
+    asteroid_field = AsteroidField()
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -19,11 +31,19 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen,(0,0,0))
-        player.draw(screen)
-        dt = clock.tick(60) / 1000
-        player.update(dt)
+        updatable.update(dt)
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+        
+        for asteroid in asteroids:
+            if asteroid.collision(player):
+                sys.exit("Game over!")
+        
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
